45d744ada8b080ce0e302c542536bd480afdb3af
tolentinomarymae <tolentinomarymae1503@gmail.com>
2023-10-01 23:57:05 +0800

    authguard

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,7 @@ $routes->post('/authreg', 'AdminController::authreg');
 $routes->post('/loginauth', 'AdminController::loginauth');
 $routes->post('/products/store', 'MainController::store');
 $routes->get('/products/:id', 'MainController@show');
-$routes->match(['post', 'get'], '/admins', 'AdminController::admin');
+$routes->match(['post', 'get'], '/admins', 'AdminController::admin',['filter'=>'authGuard']);
 $routes->match(['post', 'get'], '/login', 'AdminController::login');
 $routes->match(['post', 'get'], '/register', 'AdminController::register');
+['filter'=>'authGuard'];
